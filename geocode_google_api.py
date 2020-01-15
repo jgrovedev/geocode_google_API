63b0b896b02843a94375580c90601bc6b1c0d899
jgrove90 <37494097+jgrove90@users.noreply.github.com>
2020-01-15 15:30:33 -0700

    update

--- geocode_google_api.py
+++ geocode_google_api.py
@@ -4,12 +4,12 @@ import requests
 import time
 import json
 
-df_locations = pd.read_csv('ENTER CSV FILE HERE')
+df_locations = pd.read_csv('ENTER GEOJSON FILE HERE')
 
 # function that accepts an address string, sends it to the Google API, and returns the lat-long API result
 def geocode(address):
     time.sleep(1) #pause for some duration before each request, to not hammer their server
-    url = 'https://maps.googleapis.com/maps/api/geocode/json?address={}&key=ENTER_API_KEY_HERE' #api url with placeholders
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?address={}&key=ENTER API KEY HERE' #api url with placeholders
     request = url.format(address) #fill in the placeholder with a variable
     response = requests.get(request) #send the request to the server and get the response
     data = response.json() #convert the response json string into a dict
@@ -37,11 +37,11 @@ def df_to_geojson(df, properties, lat='Latitude', lon='Longitude'):
         geojson['features'].append(feature)                             # add this feature (aka, converted dataframe row) to the list of features inside our dict
     return geojson
 
-cols = ['ENTER COLUMN NAMES WANTED IN FEATURES']  # select which columns to add to properties in geojson
-geojson_dict = df_to_geojson('df_locations', properties=cols)
+cols = ['ENTER YOUR LIST OF PROPERTIES']  # select which columns to add to properties in geojson
+geojson_dict = df_to_geojson(df_locations, properties=cols)
 
 # save the geojson result to a file
-with open('ENTER OUTPUT FILE NAME HERE', 'w') as f:
+with open('ENTER OUTPUT FILE NAME', 'w') as f:
     json.dump(geojson_dict, f, indent=2)
 
 print('Geojson file created succesfully.')
